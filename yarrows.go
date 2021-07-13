1ccf463f32cf478e80177cbd8510ce80cbeb53bf
Florian Rand <florian.rand@gmail.com>
2021-07-13 17:40:46 +0200

    Move yarrow to a different file

---
+++ yarrows.go
@@ -0,0 +1,7 @@
+package goching
+
+var yarrows = yarrow{"OYin", "OYang", "OYang", "OYang", "Yang", "Yang", "Yang", "Yang", "Yang",
+	"Yin", "Yin", "Yin", "Yin", "Yin", "Yin", "Yin",
+}
+
+var NewYarrows = yarrows.shuffle()
