30206e102234d128693c8f607cc7e57b0cf1da57
Florian Rand <florian.rand@gmail.com>
2023-04-25 18:59:43 +0200

    Update example

--- example/example.go
+++ example/example.go
@@ -7,19 +7,19 @@ import (
 )
 
 func print(reading goching.Reading) {
-	fmt.Printf("Hexagram: %v\n", *reading.Hexagram.Number)
+	fmt.Printf("Hexagram: %v\n", reading.Hexagram.Number)
 	if len(reading.Lines) > 0 {
 		fmt.Print("Lines: ")
 		for _, line := range reading.Lines {
 			fmt.Printf("%v ", line+1)
 		}
 		fmt.Println()
-		fmt.Printf("Relating: %v\n", *reading.Relating.Number)
+		fmt.Printf("Relating: %v\n", reading.Relating.Number)
 	}
 }
 
 func main() {
-	myReading := goching.CastReading(goching.NewCast)
+	myReading := goching.CastReading(goching.NewStalks())
 
 	print(*myReading)
 }
